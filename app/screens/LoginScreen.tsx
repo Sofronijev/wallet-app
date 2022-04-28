c3dbde40f8e93ef382d7814c378a81f263a72aaf
Sofronijev <sofronijev@gmail.com>
2022-04-28 09:50:07 +0200

    LoginScreen dodat usrname i password

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,15 +1,21 @@
 import { StyleSheet, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
+import { NavigationProp } from "@react-navigation/native";
 import Label from "../components/Label";
 import LabelInput from "../components/LabelInput";
 import colors from "../constants/colors";
 import CustomButton from "../components/CustomButton";
+import { RootStackParamList } from "../navigation/routes";
 
-type Props = {};
+type Props = {
+  navigation: NavigationProp<RootStackParamList, "Login">;
+};
 
-const LoginScreen = () => {
+const LoginScreen: React.FC<Props> = ({ navigation }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
       <Label style={styles.text}>Welcome</Label>
@@ -18,14 +24,24 @@ const LoginScreen = () => {
         icon={<FontAwesome name='user-o' size={24} color={colors.greenMint} />}
         placeholder='Email'
         keyboardType='email-address'
+        value={username}
+        onChangeText={setUsername}
       />
       <LabelInput
         icon={<Ionicons name='ios-key-outline' size={24} color={colors.greenMint} />}
         placeholder='Password'
         secureTextEntry
+        value={password}
+        onChangeText={setPassword}
       />
-      <CustomButton title='Log in' style={styles.button} onPress={() => {}} />
-      <TouchableOpacity>
+      <CustomButton
+        title='Log in'
+        style={styles.button}
+        onPress={() => {
+          console.log(username, password);
+        }}
+      />
+      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
         <Label style={styles.register}>Register</Label>
       </TouchableOpacity>
     </View>
