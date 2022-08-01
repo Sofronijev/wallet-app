19cb893b3a22e307800161b661af5ca0927ae05c
Sofronijev <sofronijev@gmail.com>
2022-08-01 16:52:45 +0200

    Add Expense and Income Entity

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,21 +1,23 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { Budget } from "./entities/Budget"
-import { Category } from "./entities/Category"
-import { Type } from "./entities/Type"
-import { User } from "./entities/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { configDB } from "./config";
+import { Category } from "./entities/Category";
+import { Expense } from "./entities/Expense";
+import { Income } from "./entities/Income";
+import { Type } from "./entities/Type";
+import { User } from "./entities/User";
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "wallet_db",
-    charset: "utf8_general_ci",
-    synchronize: true,
-    logging: false,
-    entities: [User, Category, Type, Budget],
-    migrations: [],
-    subscribers: [],
-})
+  type: "mysql",
+  host: configDB.host,
+  port: 3306,
+  username: configDB.user,
+  password: configDB.password,
+  database: configDB.database,
+  charset: "utf8_general_ci",
+  synchronize: true,
+  logging: false,
+  entities: [User, Category, Type, Expense, Income],
+  migrations: [],
+  subscribers: [],
+});
---
+++ server/src/entities/Expense.ts
@@ -0,0 +1,55 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Category } from "./Category";
+import { Type } from "./Type";
+import { User } from "./User";
+
+@Entity("expenses")
+export class Expense {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "decimal",
+    precision: 10,
+    scale: 2,
+  })
+  amount: string;
+
+  @Column({
+    length: 500,
+  })
+  description: string;
+
+  @Column({
+    type: "date",
+  })
+  date: string;
+
+  @ManyToOne(() => User, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+    nullable: false,
+  })
+  @JoinColumn({
+    name: "user_id",
+  })
+  userId: User;
+
+  @ManyToOne(() => Type, {
+    onUpdate: "CASCADE",
+    onDelete: "SET NULL",
+  })
+  @JoinColumn({
+    name: "type_id",
+  })
+  typeId: Type;
+
+  @ManyToOne(() => Category, {
+    onUpdate: "CASCADE",
+    onDelete: "SET NULL",
+  })
+  @JoinColumn({
+    name: "category_id",
+  })
+  categoryId: Category;
+}
