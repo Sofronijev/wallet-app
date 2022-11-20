a1c21f86aef69723f82684065ca20abc2d65ba31
Sofronijev <sofronijev@gmail.com>
2022-11-20 18:09:13 +0100

    Use expo-splash-screen insted od expo-app-loading

--- app/navigation/RootNavigator.tsx
+++ app/navigation/RootNavigator.tsx
@@ -1,36 +1,47 @@
-
-import React, { useState } from "react";
-import AppLoading from "expo-app-loading";
+import React, { useState, useEffect, useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
 import authStorage from "modules/authStorage";
 import AppNavigator from "./AppNavigator";
 import AuthNavigator from "./AuthNavigator";
 import { useAppDispatch, useAppSelector } from "store/hooks";
 import { getUserData, setUserData } from "store/reducers/userSlice";
+import { View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootNavigator = () => {
   const user = useAppSelector(getUserData);
   const dispatch = useAppDispatch();
   const [isReady, setIsReady] = useState(false);
-  const finishedRestoreUser = () => setIsReady(true);
 
   const restoreUser = async () => {
-    const user = await authStorage.getUser();
-    if (user) dispatch(setUserData(user.userData));
+    try {
+      const user = await authStorage.getUser();
+      if (user) dispatch(setUserData(user.userData));
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      setIsReady(true);
+    }
   };
-  
-  if (!isReady)
-    return (
-      <AppLoading
-        startAsync={restoreUser}
-        onFinish={finishedRestoreUser}
-        onError={finishedRestoreUser}
-      />
-    );
 
-  if (user.id) {
-    return <AppNavigator />;
-  }
-  return <AuthNavigator />;
+  useEffect(() => {
+    restoreUser();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) return null;
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      {user.id ? <AppNavigator /> : <AuthNavigator />}
+    </View>
+  );
 };
 
 export default RootNavigator;
